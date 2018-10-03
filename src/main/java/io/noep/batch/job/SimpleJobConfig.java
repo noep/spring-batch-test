bb3618f0f2155d8a4a26be49d7e18702972aa054
Taehoon Yoo <noep@naver.com>
2018-10-03 12:51:37 +0900

    db 분리, job parameter 집어넣기

--- src/main/java/io/noep/batch/job/SimpleJobConfig.java
+++ src/main/java/io/noep/batch/job/SimpleJobConfig.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,16 +30,18 @@ public class SimpleJobConfig {
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1())
+                .start(simpleStep1(null))
                 .build();
 
     }
 
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>>THIS IS STEP 1");
+                    log.info(">>>>> requestDate : {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
