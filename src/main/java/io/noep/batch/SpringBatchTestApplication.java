dfa8820708f914094b4f0e6f2eebb33b401fe76a
Taehoon Yoo <noep@naver.com>
2018-10-03 12:16:23 +0900

    initial commit

---
+++ src/main/java/io/noep/batch/SpringBatchTestApplication.java
@@ -0,0 +1,12 @@
+package io.noep.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchTestApplication.class, args);
+    }
+}
